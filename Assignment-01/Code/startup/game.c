d589553e4c8e5f1c3463b79b79452c5f3f8a85cc
Elliot Schot <s3530160@student.rmit.edu.au>
2018-08-06 14:17:59 +1000

    Completed game_CommandInit()

--- Assignment-01/Code/startup/game.c
+++ Assignment-01/Code/startup/game.c
@@ -5,7 +5,8 @@
  */
 #include "game.h"
 
-void game_PlayGame(){
+void game_PlayGame()
+{
 	char input;
 	Board board;
 	Player player;
@@ -16,39 +17,38 @@ void game_PlayGame(){
 	readInput(&input, 2, stdin);
 	if (game_AttemptLoadCommand(&board))
 	{
-		
-		if (game_AttemptInitCommand(&board, &player) )
+
+		if (game_AttemptInitCommand(&board, &player))
 		{
 			/* game_Hunt(board, player); */
 		}
-		
-		
 	}
-	printf("Set Cell Value %d\n",board[1][1]);
+	printf("Set Cell Value %d\n", board[1][1]);
 	board_Display(board);
 }
 
 /* DONE */
-int game_DisplayOptions(){
-    return printf(
-    "You can use the following commands to play the game: \n"
-	"\n"
-	"load <g> \n"
-	" g: number of the game board to load (either 1 or 2)\n"
-	"init <x>,<y>\n"
-	" x: horizontal position of the player on the board (between 0 & 4) \n"
-	" y: vertical position of the player on the board (between 0 & 4) \n"
-	"north (or n) \n"
-	"south (or s) \n"
-	"east (or e) \n"
-	"west (or w) \n"
-	"quit \n"
-	"\n"
-	"Press enter to continue... "
-    );
+int game_DisplayOptions()
+{
+	return printf(
+		"You can use the following commands to play the game: \n"
+		"\n"
+		"load <g> \n"
+		" g: number of the game board to load (either 1 or 2)\n"
+		"init <x>,<y>\n"
+		" x: horizontal position of the player on the board (between 0 & 4) \n"
+		" y: vertical position of the player on the board (between 0 & 4) \n"
+		"north (or n) \n"
+		"south (or s) \n"
+		"east (or e) \n"
+		"west (or w) \n"
+		"quit \n"
+		"\n"
+		"Press enter to continue... ");
 }
 
-Boolean game_AttemptLoadCommand(Board * board){
+Boolean game_AttemptLoadCommand(Board *board)
+{
 	char input;
 	char *promptMessage;
 
@@ -59,19 +59,19 @@ Boolean game_AttemptLoadCommand(Board * board){
 		{
 			getInput(promptMessage, &input, 52);
 			putchar(10);
-			if ( !strcmp(&input, "quit") )
+			if (!strcmp(&input, "quit"))
 				return FALSE;
-			if ( !strncmp(&input, "load", 4) )
+			if (!strncmp(&input, "load", 4))
 				break;
 			printInvalidInput();
 		}
 		/*break;*/
-		
+
 		if (game_CommandLoad(&input, board))
 		{
 			break;
 		}
-		
+
 		printInvalidInput();
 	}
 
@@ -79,7 +79,7 @@ Boolean game_AttemptLoadCommand(Board * board){
 	return TRUE;
 }
 
-Boolean game_CommandLoad(char * loadSelection, Board * board)
+Boolean game_CommandLoad(char *loadSelection, Board *board)
 {
 	board_Load(board, &BOARD_1);
 	return TRUE;
@@ -102,7 +102,7 @@ Boolean game_CommandLoad(char * loadSelection, Board * board)
 		{
 		if ( selectionInt == 1 )
 		{
-			board_Load(a2, &BOARD_1);
+			board_Load(board, &BOARD_1);
 		}
 		else
 		{
@@ -110,7 +110,7 @@ Boolean game_CommandLoad(char * loadSelection, Board * board)
 			{
 				assert(0);
 			}
-			board_Load(a2, &BOARD_2);
+			board_Load(board, &BOARD_2);
 		}
 		return TRUE;
 		}
@@ -122,48 +122,100 @@ Boolean game_CommandLoad(char * loadSelection, Board * board)
 	*/
 }
 
-Boolean game_AttemptInitCommand(Board * board, Player * player)
+Boolean game_AttemptInitCommand(Board *board, Player *player)
 {
-  	char input;
-  	int selectionInt;
+	char input;
+	int selectionInt;
 	char *promptMessage;
- 
+
 	promptMessage = "At this stage of the program, only two commands are acceptable: \ninit <x>,<y> \nquit \n\n";
 	do
 	{
 		while (TRUE)
 		{
-		getInput(promptMessage, &input, 52);
-		if ( !strcmp(&input, "quit") )
-			return FALSE;
-		if ( !strncmp(&input, "init", 4) )
-			break;
-		printInvalidInput();
+			getInput(promptMessage, &input, 52);
+			if (!strcmp(&input, "quit"))
+				return FALSE;
+			if (!strncmp(&input, "init", 4))
+				break;
+			printInvalidInput();
 		}
 		selectionInt = game_CommandInit(&input, board, player);
-	}
-	while ( selectionInt != 1 );
+	} while (selectionInt != 1);
 	return TRUE;
 }
 
-Boolean game_CommandInit(char * input, Board * board, Player * player){
-  	Boolean placePlayerResult;
+Boolean game_CommandInit(char *input, Board *board, Player *player)
+{
+	Boolean placePlayerResult;
 	Boolean result;
 	Position selectedPosition;
+	Player *localPlayer;
+	char *endptr;
+	char *nptr;
 
-	selectedPosition.x = 1;
-	selectedPosition.y = 1;
+	printf("Debug Print 1.1: Input = %s\n", input);
 
-	placePlayerResult = board_PlacePlayer(board, selectedPosition);
-	if ( placePlayerResult )
+	localPlayer = player;
+	strtok(input, " ");
+
+	printf("Debug Print 1.2: Input = %s\n", input);
+	nptr = strtok(NULL, ",");
+
+	printf("Debug Print 1.3: nptr = %s\n", nptr);
+	if (nptr)
 	{
-		player_Initialise(player, selectedPosition);
+		selectedPosition.x = strtol(nptr, &endptr, 10);
+		printf("Debug Print 2: X = %d\n", selectedPosition.x);
+		if (nptr == endptr || *endptr || selectedPosition.x < 0 || selectedPosition.x > BOARD_WIDTH)
+		{
+			printf("Debug Print 2.2: %s, %s, %d, %d\n", nptr, endptr, selectedPosition.x, BOARD_WIDTH);
+			printf("Debug Print 2.3: Exit 1\n");
+			printInvalidInput();
+			result = FALSE;
+		}
+		else
+		{
+			nptr = strtok(NULL, ",");
+			printf("Debug Print 2.4: nptr = %s\n", nptr);
+			if (nptr)
+			{
+				selectedPosition.y = strtol(nptr, &endptr, 10);
+				printf("Debug Print 3: Y = %d\n", selectedPosition.y);
+				if (nptr == endptr || *endptr || selectedPosition.y < 0 || selectedPosition.y > BOARD_HEIGHT)
+				{
+					printf("Debug Print 3.1: Exit 1\n");
+					printInvalidInput();
+					result = FALSE;
+				}
+				else
+				{
+					printf("Debug Print 4: Placing Player\n");
+					placePlayerResult = board_PlacePlayer(board, selectedPosition);
+					if (placePlayerResult)
+					{
+						printf("Debug Print 4.1: Placing Player Result: %d\n", placePlayerResult);
+						player_Initialise(localPlayer, selectedPosition);
+					}
+					else
+					{
+						printf("Unable to place player at position x: %d, y: %d \n", selectedPosition.x, selectedPosition.y);
+					}
+					result = placePlayerResult;
+				}
+			}
+			else
+			{
+				printf("Debug Print 5: Exit 1\n");
+				printInvalidInput();
+				result = FALSE;
+			}
+		}
 	}
 	else
 	{
-		printf("Unable to place player at that position. \n");
+		printInvalidInput();
+		result = FALSE;
 	}
-	result = placePlayerResult;
 	return result;
-	/* TODO REST */
 }
