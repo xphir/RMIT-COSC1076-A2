0c2d7e40ec918c82b4a233020ea6f6d1bb4b4f09
Elliot Schot <s3530160@student.rmit.edu.au>
2018-09-04 15:28:39 +1000

    Adding A2 Docs

---
+++ Assignment-02/startup/invector.c
@@ -0,0 +1,13 @@
+/******************************************************************************
+** Student name: 	...
+** Student number: 	...
+** Course: 			Advanced Programming Techniques - S2 2018
+******************************************************************************/
+
+#include "invector.h"
+
+void invector_init(InVTable *invt) {
+	/* TO DO */
+}
+
+
