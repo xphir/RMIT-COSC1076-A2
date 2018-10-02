a545ee81773e86dc185785ff8f453090ce05d0a1
Elliot Schot <s3530160@student.rmit.edu.au>
2018-10-02 20:59:37 +1000

    complete gene_create_rand_gene()

--- Assignment-02/startup/gene.c
+++ Assignment-02/startup/gene.c
@@ -46,30 +46,53 @@ double eval_minfn(InVTable *invt, Gene *gene){
 	return 0.0;
 }
 
-Gene * gene_create_rand_gene(int numAlleles, CreateFn create_chrom){
+Gene *gene_create_rand_gene(int numAlleles, CreateFn create_chrom)
+{
 	/* TO DO */
-	return NULL;
+	int chromosome;
+	Gene *result_gene;
+	Gene *new_gene = malloc(sizeof(*new_gene));
+
+	chromosome = create_chrom(numAlleles);
+
+	if (new_gene != NULL)
+	{
+		new_gene->chromosome = chromosome;
+		new_gene->num_alleles = numAlleles;
+		new_gene->raw_score = 0;
+		new_gene->fitness = 0;
+		result_gene = new_gene;
+	}
+	else
+	{
+		fwrite("Unable to allocate memory in gene_create_rand_gene\n", 1, 51, stderr);
+		result_gene = NULL;
+	}
+	return result_gene;
 }
 
-void gene_calc_fitness(Gene *gene, EvalFn evaluate_fn, InVTable *invTab){
+void gene_calc_fitness(Gene *gene, EvalFn evaluate_fn, InVTable *invTab)
+{
 	/* TO DO */
 }
 
-void gene_normalise_fitness(Gene *gene, double total_fitness){
+void gene_normalise_fitness(Gene *gene, double total_fitness)
+{
 	/* TO DO */
 }
 
-void gene_free(Gene *gene){
+void gene_free(Gene *gene)
+{
 	/* TO DO */
 }
 
-
-double gene_get_fitness(Gene *gene){
+double gene_get_fitness(Gene *gene)
+{
 	/* TO DO */
 	return 0.0;
 }
 
-void gene_print(Gene *gene) {
+void gene_print(Gene *gene)
+{
 	/* TO DO */
 }
-
