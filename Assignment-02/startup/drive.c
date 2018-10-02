09afb77e404905dc4a0d0e023387b8701f661d72
Elliot Schot <s3530160@student.rmit.edu.au>
2018-10-02 22:57:19 +1000

    finish test

--- Assignment-02/startup/drive.c
+++ Assignment-02/startup/drive.c
@@ -9,49 +9,80 @@
 #include "pop.h"
 #include "gene.h"
 
-void test_pcbmill(void){
+void test_pcbmill(void)
+{
+	Gene *pcbGene;
+	Gene *mutantGene;
+
+	Gene *parentGeneA;
+	Gene *parentGeneB;
+	Gene *childGene;
+
 	/* TO DO */
 
 	printf("PCBMILL gene:\n");
 	/* TO DO - create a random pcbmill gene by calling create_rand_gene
 	   The length of the gene's chromosome should be TEST_ALLELE_LEN */
-	
-	Gene * pcbGene;
 	CreateFn chrom_pointer = &create_pcbmill_chrom;
 	pcbGene = gene_create_rand_gene(TEST_ALLELE_LEN, chrom_pointer);
-	
+
 	/* TO DO - print the gene using gene_print */
 	gene_print(pcbGene);
-	
 
 	printf("\nMutate: ");
 	/* TO DO - create a mutant copy of the gene using mutate_pcbmill */
-	
-	/* TO DO - print the mutant gene using gene_print */
+	MutateFn mutate_pointer = &mutate_pcbmill;
+	mutantGene = mutate_pointer(pcbGene);
 
+	/* TO DO - print the mutant gene using gene_print */
+	gene_print(mutantGene);
 	printf("\n");
+
 	/* TO DO - free the original gene and the mutant */
-	free(pcbGene)
+	gene_free(pcbGene);
+	gene_free(mutantGene);
+
 	printf("PCBMILL genes:\n");
 	/* TO DO - create 2 random pcbmill 'parent' genes using calls
 	   to create_rand_gene
 	   The length of the gene's chromosome should be TEST_ALLELE_LEN */
+	parentGeneA = gene_create_rand_gene(TEST_ALLELE_LEN, chrom_pointer);
+	parentGeneB = gene_create_rand_gene(TEST_ALLELE_LEN, chrom_pointer);
+
 	/* TO DO - print each gene */
+	gene_print(parentGeneA);
+	gene_print(parentGeneB);
+
 	printf("\nCrossover: ");
 	/* TO DO produce a new gene by calling crossover_pcbmill
 	   with the parent genes */
+	CrossOverFn crossover_pointer = &crossover_pcbmill;
+	childGene = crossover_pointer(parentGeneA, parentGeneB);
+
 	/* TO DO - print the new gene */
+	gene_print(childGene);
+
 	printf("\n");
 	/* TO DO - free both parents and the child gene */
+	gene_free(parentGeneA);
+	gene_free(parentGeneB);
+	gene_free(childGene);
 }
 
-void test_minfn(void){
+void test_minfn(void)
+{
+	Gene *pcbGene;
+	Gene *mutantGene;
+
+	Gene *parentGeneA;
+	Gene *parentGeneB;
+	Gene *childGene;
 	/* TO DO */
 
 	printf("MINFN gene:\n");
 	/* TO DO - create a random minfn gene by calling create_rand_gene
 	   The length of the gene's chromosome should be TEST_ALLELE_LEN */
-	Gene * pcbGene;
+
 	CreateFn chrom_pointer = &create_minfn_chrom;
 	pcbGene = gene_create_rand_gene(TEST_ALLELE_LEN, chrom_pointer);
 
@@ -60,34 +91,53 @@ void test_minfn(void){
 
 	printf("\nMutate: ");
 	/* TO DO - create a mutant copy of the gene using mutate_minfn */
+	MutateFn mutate_pointer = &mutate_minfn;
+	mutantGene = mutate_pointer(pcbGene);
 	/* TO DO - print the mutant gene */
+	gene_print(mutantGene);
 	printf("\n");
 
-	free(pcbGene)
 	/* TO DO - free the original gene and the mutant */
+	gene_free(pcbGene);
+	gene_free(mutantGene);
+
 	printf("MINFN genes:\n");
 	/* TO DO - create 2 random minfn 'parent' genes using calls
 	   to create_rand_gene
 	   The length of the gene's chromosome should be TEST_ALLELE_LEN */
+	parentGeneA = gene_create_rand_gene(TEST_ALLELE_LEN, chrom_pointer);
+	parentGeneB = gene_create_rand_gene(TEST_ALLELE_LEN, chrom_pointer);
+
 	/* TO DO - print each gene */
+	gene_print(parentGeneA);
+	gene_print(parentGeneB);
+
 	printf("\nCrossover: ");
 	/* TO DO produce a new gene by calling crossover_minfn
 	   with the parent genes */
+	CrossOverFn crossover_pointer = &crossover_minfn;
+	childGene = crossover_pointer(parentGeneA, parentGeneB);
+
 	/* TO DO - print the new gene */
+	gene_print(childGene);
+
 	printf("\n");
 	/* TO DO - free both parents and the child gene */
+	gene_free(parentGeneA);
+	gene_free(parentGeneB);
+	gene_free(childGene);
 }
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
 
 	/* TO DO */
 
 	/* The only point at which srand should be called */
 	srand(SRAND_SEED);
 
-	void test_minfn(void);
-	void test_pcbmill(void);
+	test_minfn();
+	test_pcbmill();
 
 	return EXIT_SUCCESS;
 }
-
